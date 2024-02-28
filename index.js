95d69428c7d6d67f51998f9e6e6e0480fce8b5fc
Eileen <eileenburdekin@proton.me>
2024-02-27 16:11:41 -0800

    use localstorage to persist logged in state

--- index.js
+++ index.js
@@ -85,11 +85,12 @@ app.get("/callback", (req, res) => {
         //     res.send(error);
         //   });
 
-        const { access_token, refresh_token } = response.data;
+        const { access_token, refresh_token, expires_in } = response.data;
 
         const queryParams = querystring.stringify({
           access_token,
           refresh_token,
+          expires_in,
         });
 
         // redirect to react app - refreshed page will have URL with token values as query params
