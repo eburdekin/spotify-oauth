96847c5db7921cb1b17b786b8142bbb6b120f4e2
Eileen <eileenburdekin@proton.me>
2024-02-27 16:22:13 -0800

    show basic profile data in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import { useState, useEffect } from "react";
-import { accessToken, logout } from "./Spotify";
+import { accessToken, logout, getCurrentUserProfile } from "./Spotify";
+import { catchErrors } from "./utils";
 import "./App.css";
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        // need to make sure errors are handled during async function
+        setProfile(data);
+        console.log(data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    catchErrors(fetchData());
   }, []);
 
   return (
@@ -20,6 +35,16 @@ function App() {
           <>
             <h1>Logged in!</h1>
             <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[1].url && (
+                  <img src={profile.images[1].url} alt="Avatar" />
+                )}
+              </>
+            )}
           </>
         )}
       </header>
