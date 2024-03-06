cd5a09058dc0d00bcd56a2593bb5bb0f451a8785
Eileen <eileenburdekin@proton.me>
2024-03-06 12:49:58 -0800

    style login button

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,20 +1,35 @@
 import styled from "styled-components";
 
-const StyledLoginButton = styled.a`
-  background-color: green;
-  color: white;
-  padding: 10px 20px;
-  margin: 20px auto;
-  border-radius: 30px;
-  display: inline-block;
+const StyledLoginContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
 
-const Login = () => {
-  return (
+const StyledLoginButton = styled.a`
+  display: inline-block;
+  background-color: var(--green);
+  color: var(--white);
+  border-radius: var(--border-radius-pill);
+  font-weight: 700;
+  font-size: var(--fz-lg);
+  padding: var(--spacing-sm) var(--spacing-xl);
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    filter: brightness(1.1);
+  }
+`;
+
+const Login = () => (
+  <StyledLoginContainer>
     <StyledLoginButton href="http://localhost:8888/login">
       Log in to Spotify
     </StyledLoginButton>
-  );
-};
+  </StyledLoginContainer>
+);
 
 export default Login;
